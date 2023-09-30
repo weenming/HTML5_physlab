0668a720c41b13a7c04ea3435f7e2ee492952a1f
ruiqi <rqfeng20@fudan.edu.cn>
2023-09-30 21:23:48 +0800

    nn

--- angular_spectrum/sim/layer.py
+++ angular_spectrum/sim/layer.py
@@ -1,15 +1,30 @@
+from torch import nn
+import torch
 
-class Layer:
-    def __init__(self, ot, pixel, z, **kwargs):
+class SLM(nn.Module):
+    def __init__(self, pixel, grid, X=None, **kwargs):
         '''
-        z: distance from the LAST layer 
-            if first layer, then the distance from the objective
+        pixel: resolution of the SLM
+        grid: spatial discretization size in the simulation
         '''
-        self.X = ot
-        self.z = z
         self.pixel = pixel
+        self.grid = grid
+
+        self.X = nn.Parameter(torch.zeros((pixel, pixel)))
+        if X is None:
+            self.reset_parameters()
+        else:
+            assert X.size() == self.X.size(), 'bad initializer'
+            self.X = X
+        assert self.pixel > self.grid
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.X) * 2 * torch.pi
     
-    def phase_modulate(self, X, pixel_x, wl):
+    def _X_on_grid(self, X):
+        change_resolution(X, self.pixel, self.grid)
+
+    def phase_modulate(self, pixel_x, wl):
         assert (pixel_x - self.pixel).abs() < 1e-10, 'not only support same \
             resolution between image and layer.'
         raise NotImplementedError
---
+++ angular_spectrum/util/resolution.py
@@ -0,0 +1,11 @@
+
+def change_resolution(X, before_x, before_y, after_x, after_y):
+    '''
+    before / after: pixel size before and after the transform
+    '''
+    x_ratio = after_x / before_x
+    y_ratio = after_y / before_y
+
+    
+
+    
