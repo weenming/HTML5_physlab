1ca95ad5d360c3b58d8f7c41272554f637888b4b
喵喵喵喵喵w <2311602492@qq.com>
2023-11-29 16:50:41 +0800

    automation

--- app.py
+++ app.py
@@ -1,32 +1,101 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory
 
 import sys
-#sys.path.append('./../../')
+sys.path.append('angular_spectrum/')
 
-#import angular_spectrum.sim.propagate as prop
+import sim.propagate as prop
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import webbrowser
 
 plt.switch_backend('Agg')
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-def perform_simulation(z):
-    # Replace this with your actual simulation logic using Matplotlib
-    x = np.linspace(0, 10, 100)
-    y = np.sin(z * x)
+def get_circle(halfn):
+    r = 10
+    X = torch.arange(-halfn, halfn) ** 2 + torch.arange(-halfn, halfn).unsqueeze(-1) ** 2
+    idx = X < r ** 2
+    n_idx = X >= r ** 2
 
-    plt.plot(x, y)
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Simulation Plot')
+    X[n_idx] = 0
+    X[idx] = 1
+    return X
+
+def get_square(halfn):
+    s = 10
+    idx = torch.arange(halfn - s, halfn + s)
+    idx = (idx.repeat(2 * s), 
+        idx.repeat_interleave(2 * s))
     
-    # Save the plot as an image in the static folder
+
+    X = torch.zeros((halfn * 2, halfn * 2))
+    X[idx] = 1
+    return X
+
+def get_double_slit(halfn):
+    w = 10
+    d = 2
+    x_left_idx = torch.arange(halfn - w // 2 - d // 2, halfn - w // 2 + d // 2)
+    x_right_idx = torch.arange(halfn + w // 2 - d // 2, halfn + w // 2 + d // 2)
+    x_left_idx = x_left_idx.repeat(halfn * 2)
+    x_right_idx = x_right_idx.repeat(halfn * 2)
+    y_idx = torch.arange(0, halfn * 2).repeat_interleave(d)
+      
     
+    X = torch.zeros((halfn * 2, halfn * 2))
+    X[x_left_idx, y_idx] = 1
+    X[x_right_idx, y_idx] = 1
+    return X
+
+def get_obj(get_x=get_circle):
+    n = 1000 # <~10000
+    halfn = n // 2
+    X = get_x(halfn)
+
+    X = X / X.max() # normalize
+    X = X.to(torch.complex128) # NOTE: complex 64 induces accumulated error which is catastrophic
+    return X
+
+# preprocess
+
+def cut(X, show_ratio):
+    halfn = X.shape[0] // 2
+    lower = int(halfn * (1 - show_ratio))
+    higher = int(halfn * (1 + show_ratio))
+    
+    return X[lower: higher, lower:higher]
+
+def show_mat(X, pixel, show_ratio=1):
+    X = cut(X, show_ratio)
+    halfn = X.shape[0] // 2
+    
+    X = X.abs().cpu().numpy()
+
+    fig, ax = plt.subplots(1, 1)
+    s = ax.imshow(
+        X, 
+        extent=[-halfn * pixel, halfn * pixel, -halfn * pixel, halfn * pixel]
+    )
+    ax.set_xlabel('$\mu$m')
+    ax.set_ylabel('$\mu$m')
+    ax.set_title('objective')
+    fig.colorbar(s)
+
+    return
+
+def perform_simulation(z, wl, grid):
+    X = prop.propagate(get_obj(get_double_slit), grid, wl, z)
+
+    show_mat(get_obj(get_double_slit), grid, show_ratio=1)
+    plt.savefig(os.path.join(app.static_folder, 'start_plot.png'))
+    plt.close()
+
+    show_mat(X, grid, show_ratio=1)
     plt.savefig(os.path.join(app.static_folder, 'simulation_plot.png'))
-    plt.close()  # Close the plot to free up resources
+    plt.close()
 
     return 'static/simulation_plot.png'
 
@@ -40,9 +109,11 @@ def run_simulation():
     # Get input parameters from the request
     data = request.get_json()
     z = float(data.get('z'))
+    wl = float(data.get('wl'))
+    grid = float(data.get('grid'))
 
     # Perform the optical simulation and get the path of the saved plot
-    image_path = perform_simulation(z)
+    image_path = perform_simulation(z, wl, grid)
 
     # Return the path of the saved image to the web page
     return jsonify({'image_path': image_path})
@@ -52,4 +123,6 @@ def serve_image(filename):
     return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    webbrowser.open("http://127.0.0.1:8080")
+    app.run(debug=False, port=8080)
+    
